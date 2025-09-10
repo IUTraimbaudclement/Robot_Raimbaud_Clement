66ab0ee4bd2054920168c31d8a72f2492740dfe9
iutgeiitoulonE105 <touloniut@gmail.com>
2025-09-10 11:49:13 +0200

    0

---
+++ Embedded/Robot_Raimbaud_Clement.X/PWM.h
@@ -0,0 +1,11 @@
+#ifndef PWM_H
+#define PWM_H
+
+#define MOTEUR_DROIT 0
+#define MOTEUR_GAUCHE 1
+
+void InitPWM(void);
+void PWMSetSpeed(int, float);
+void PWMUpdateSpeed(void);
+
+#endif /* PWM_H */
---
+++ Embedded/Robot_Raimbaud_Clement.X/build/default/production/main.o.d
@@ -0,0 +1,9 @@
+build/default/production/main.o: main.c ChipConfig.h IO.h timer.h pwm.h
+
+ChipConfig.h:
+
+IO.h:
+
+timer.h:
+
+pwm.h:
